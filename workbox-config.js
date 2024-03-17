b58ef4fe3fcd9eb5fdb8d235b0c9706a4121f355
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2024-03-17 12:35:00 +0300

    update the workbox

--- workbox-config.js
+++ workbox-config.js
@@ -1,12 +1,13 @@
 module.exports = {
-	globDirectory: './',
+	globDirectory: '.',
 	globPatterns: [
 		'**/*.{png,json,css,ico,eot,svg,ttf,woff,txt,scss,html,pdf,js,jpg,gif,md,mp3,wav}'
 	],
 	swDest: 'sw.js',
+	
+	maximumFileSizeToCacheInBytes: 5000000,
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
-	],
-	maximumFileSizeToCacheInBytes: 5000000
+	]
 };
