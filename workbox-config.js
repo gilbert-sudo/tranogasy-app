b495a8b930e0baf528bd42d075aedb829df1249a
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2024-03-17 13:51:06 +0300

    try a new workbox-config

--- workbox-config.js
+++ workbox-config.js
@@ -1,13 +1,14 @@
 module.exports = {
-	globDirectory: '.',
-	globPatterns: [
-		'**/*.{png,json,css,ico,eot,svg,ttf,woff,txt,scss,html,pdf,js,jpg,gif,md,mp3,wav}'
-	],
-	swDest: 'sw.js',
-	
-	maximumFileSizeToCacheInBytes: 5000000,
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+    globDirectory: './',
+    globPatterns: [
+        '**/*.{png,json,css,ico,eot,svg,ttf,woff,txt,scss,html,pdf,js,jpg,gif,md,mp3,wav,manifest}' // Include manifest.json
+    ],
+    swDest: 'sw.js',
+    clientsClaim: true,
+    skipWaiting: true,
+    maximumFileSizeToCacheInBytes: 5000000,
+    ignoreURLParametersMatching: [
+        /^utm_/,
+        /^fbclid$/
+    ]
 };
