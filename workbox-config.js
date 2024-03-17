df05cde4ae280b3c217d1642b5490d539fde48ae
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2024-03-17 16:41:41 +0300

    keep updating untill success

--- workbox-config.js
+++ workbox-config.js
@@ -1,14 +1,24 @@
 module.exports = {
-    globDirectory: './',
-    globPatterns: [
-        '**/*.{png,json,css,ico,eot,svg,ttf,woff,txt,scss,html,pdf,js,jpg,gif,md,mp3,wav,manifest}' // Include manifest.json
-    ],
-    swDest: 'sw.js',
-    clientsClaim: true,
-    skipWaiting: true,
-    maximumFileSizeToCacheInBytes: 5000000,
-    ignoreURLParametersMatching: [
-        /^utm_/,
-        /^fbclid$/
-    ]
+  globDirectory: ".",
+  globPatterns: [
+    "**/*.{png,json,css,ico,eot,svg,ttf,woff,txt,scss,html,pdf,js,jpg,gif,md,mp3,wav,manifest}", // Include manifest.json
+  ],
+  swDest: "sw.js",
+  runtimeCaching: [
+    {
+      urlPattern: ({ url }) => url.origin === "https://gotrano.onrender.com/",
+      handler: "CacheFirst",
+      options: {
+        cacheName: "cdn-assets",
+        expiration: {
+          // Adjust the number of entries to keep as needed.
+          maxEntries: 50,
+        },
+      },
+    },
+  ],
+  clientsClaim: true,
+  skipWaiting: true,
+  maximumFileSizeToCacheInBytes: 5000000,
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
